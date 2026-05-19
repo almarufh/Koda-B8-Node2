605ee04d80abb453a1b712e0297be0774a05c5d0
ALMA'RUF HIDAYAT <hidayatmaruf99@gmail.com>
2026-05-19 16:29:21 +0800

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    languageOptions: { 
+      globals: globals.node 
+    },
+    rules: {
+      semi: ["error", "always"], // Boleh tanpa kutip
+      indent: ["error", 2],      // Boleh tanpa kutip
+      
+      // WAJIB pakai kutip karena ada tanda strip (-)
+      "brace-style": ["error", "1tbs", { allowSingleLine: false }],
+      "object-curly-newline": ["error", {
+        ObjectExpression: { multiline: true, minProperties: 2 },
+        ObjectPattern: { multiline: true },
+        ImportDeclaration: { multiline: true, minProperties: 3 },
+        ExportDeclaration: { multiline: true, minProperties: 3 }
+      }],
+      "object-property-newline": ["error", { allowAllPropertiesOnSameLine: false }],
+      "array-bracket-newline": ["error", { multiline: true, minItems: 2 }],
+      "array-element-newline": ["error", "always"],
+      "max-statements-per-line": ["error", { max: 1 }]
+    } 
+  }
+];
