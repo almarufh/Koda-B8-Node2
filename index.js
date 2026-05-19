bd0a2db696ad24da7c8d9417e877643d11998cf0
ALMA'RUF HIDAYAT <hidayatmaruf99@gmail.com>
2026-05-19 16:14:24 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-console.clear()
+console.clear();
 import changeDate from "./src/manual.js";
-import withMoment from "./src/moment.js"
+import withMoment from "./src/moment.js";
 import {createInterface} from "node:readline";
 
 const cli = createInterface({
@@ -8,18 +8,75 @@ const cli = createInterface({
   output: process.stdout,
 });
 
-
-
-cli.question("Input tanggal : ", function (input) {
-    console.clear()
-    let usemoment = withMoment(input);
-    let notUse = changeDate(input)
-    if (usemoment !== notUse) {
-        console.log("Format tanggal salah");
-        return; 
+function main () {
+  let message = `Selamat Datang Dalam Program mengubah Format tanggal dari "DD-MM-YYYY" to 'DD/MM/YYYY'\n\n`;
+  message += `1. With External Package\n`;
+  message += `2. Without External Package\n`;
+  message += `\nExit\n`;
+  cli.question(message + "\n\nPilih (1-2)/Exit : ", function (input) {
+    console.clear();
+    switch (input.toUpperCase()) {
+    case "1": {
+      fwithMoment ();
+      break;
     }
-    console.log(`\nResponse change date whit moment :`, usemoment);
-    console.log("\n")
-    console.log(`Response change date whitout moment :`, notUse)
-})
-cli.close;
+    case "2": {
+      withoutMoment();
+      break;
+    }
+    case "EXIT": {
+      cli.close();
+      break;
+    }
+    default: {
+      main();
+      break;
+    }
+    }
+  });
+}
+
+
+function fwithMoment () {
+  console.clear();
+  cli.question("\nInput tanggal format DD-MM-YYYY :", function (input) {
+    let usemoment = withMoment(input);
+    let notUse = changeDate(input);
+    if (usemoment !== notUse) {
+      console.clear();
+      console.log("\n\nFormat tanggal salah");
+      backToMenu();
+      return;
+    }
+    console.clear();
+    console.log(`\nResponse change date whith moment :`, usemoment);
+    backToMenu();
+  });
+}
+
+function withoutMoment () {
+  cli.question("Input tanggal format DD-MM-YYYY : ", function (input) {
+    let usemoment = withMoment(input);
+    let notUse = changeDate(input);
+    if (usemoment !== notUse) {
+      console.clear();
+      console.log("\n\nFormat tanggal salah\n\n");
+      backToMenu();
+      return;
+    }
+    console.clear();
+    console.log(`\nResponse change date whithout moment :`, notUse);
+    backToMenu();
+  });
+}
+
+function backToMenu() {
+  cli.question("Kembali ke Menu Awal y/n : ", function (input) {
+    if (input.toUpperCase() !== "Y") {
+      cli.close();
+      return;
+    }
+    main();
+  });
+}
+main();
